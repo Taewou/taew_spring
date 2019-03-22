85397589173f0d14d4dc7e58bab9d4a04b3a1f92
Taewou <48584878+Taewou@users.noreply.github.com>
2019-03-23 00:28:42 +0900

    Souce Plus

---
+++ README.md
@@ -0,0 +1,1 @@
+# taewou_spring
---
+++ src/main/java/com/taewou/jdbc/spring/Greeter.java
@@ -0,0 +1,13 @@
+package com.taewou.jdbc.spring;
+
+public class Greeter {
+
+	public Greeter() {
+		System.out.println("Greeter instance created.");
+	}
+	
+	public String greet() {
+		return "Hello, World!";
+	}
+}
+
